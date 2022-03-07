95ea1376ea539f2f1ac29cea4ee8f46b5e821f07
kevo <kevomusa6565@gmail.com>
2022-03-07 14:23:28 +0300

    struts

--- 11mystruts/main.go
+++ 11mystruts/main.go
@@ -5,4 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println("Learning struts")
 
+	kevo := User{"Kelvin", "kelvin@dawascope.com", true, 23}
+
+	fmt.Println(kevo)
+	fmt.Printf("All details are %+v \n", kevo)
+	fmt.Printf("Kevo's name is %v and email is %v \n", kevo.Name, kevo.Email)
+}
+
+type User struct {
+	Name   string
+	Email  string
+	Status bool
+	Age    int
 }
